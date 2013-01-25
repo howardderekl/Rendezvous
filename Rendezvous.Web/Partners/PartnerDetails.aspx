1f49afb21d31f31f304320c1c6ce9a2eb49caa70
howardderekl <howard_derek@hotmail.com>
2013-01-24 21:12:00 -0700

    started on partner details.

---
+++ Rendezvous.Web/Partners/PartnerDetails.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/Marketing.Master" AutoEventWireup="true" CodeBehind="PartnerDetails.aspx.cs" Inherits="Rendezvous.Web.Partners.PartnerDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphHead" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphBody" runat="server">
+</asp:Content>
---
+++ Rendezvous.Web/Partners/PartnerDetails.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rendezvous.Web.Core;
+using Rendezvous.Web.Core.Routing;
+
+namespace Rendezvous.Web.Partners
+{
+    public partial class PartnerDetails : PreLoginBasePage, IPageWithId
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int CurrentPartnerKey {
+            get { return UrlId; }
+        }
+
+        public int UrlId { get; set; }
+    }
+}
